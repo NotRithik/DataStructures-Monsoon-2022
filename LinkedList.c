df7af8994e524ee4259cd520b45944198ef1708b
NotRithik <rithikum123@gmail.com>
2022-10-05 23:35:24 +0530

    Create function to add a node after current node to the LinkedList

--- LinkedList.c
+++ LinkedList.c
@@ -9,19 +9,30 @@ struct LinkedList {
 
 typedef struct LinkedList Node; // Define type Node
 
+void addNodeNext(int, Node *);
+
 void main()
 {
-    Node *nodeA = malloc(sizeof(Node));
+    Node *nodeA = malloc(sizeof(Node)); // First element in LinkedList
     nodeA->value = 13;
 
-    nodeA->next = malloc(sizeof(Node));
+    nodeA->next = malloc(sizeof(Node)); // Second element in LinkedList
     nodeA->next->value = 69;
 
-    nodeA->next->next = NULL;
+    nodeA->next->next = NULL; // Make second element's 'next' pointer point to NULL so we know it is the end
+
+    addNodeNext(20, nodeA);
 
     while (nodeA != NULL)
     {
         printf("%d\n", nodeA->value);
         nodeA = nodeA->next;
     }
+}
+
+void addNodeNext(int value, Node *node) {
+    Node *temp = node -> next;
+    node -> next = malloc(sizeof(Node));
+    node -> next -> next = temp;
+    node -> next -> value = value;
 }
