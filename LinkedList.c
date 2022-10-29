3ae3314917173c6c1f7506e766670ba0605976a6
NotRithik <rithikum123@gmail.com>
2022-10-29 18:38:00 +0530

    Create function to create LinkedList from int array given n where n is the size of the int array

--- LinkedList.c
+++ LinkedList.c
@@ -12,25 +12,39 @@ typedef struct LinkedList Node; // Define type Node
 
 void addNodeNext(int, Node *);
 void delNextNode(Node *);
+Node * createLinkedList(int *, int);
 
 void main()
 {
-    Node *nodeA = malloc(sizeof(Node)); // First element in LinkedList
-    nodeA->value = 13;
+    // Node *nodeA = malloc(sizeof(Node)); // First element in LinkedList
+    // nodeA->value = 13;
 
-    nodeA->next = malloc(sizeof(Node)); // Second element in LinkedList
-    nodeA->next->value = 69;
+    // nodeA->next = malloc(sizeof(Node)); // Second element in LinkedList
+    // nodeA->next->value = 69;
 
-    nodeA->next->next = NULL; // Make second element's 'next' pointer point to NULL so we know it is the end
+    // nodeA->next->next = NULL; // Make second element's 'next' pointer point to NULL so we know it is the end
 
-    addNodeNext(20, nodeA);
-    addNodeNext(20, nodeA->next);
-    delNextNode(nodeA);
+    // addNodeNext(20, nodeA);
+    // addNodeNext(20, nodeA->next);
+    // delNextNode(nodeA);
 
-    while (nodeA != NULL)
+    // while (nodeA != NULL)
+    // {
+    //     printf("%d\n", nodeA->value);
+    //     nodeA = nodeA->next;
+    // }
+
+    int n = 10, something[n];
+    for (int i = 0; i < n; i++)
     {
-        printf("%d\n", nodeA->value);
-        nodeA = nodeA->next;
+        something[i] = i+1;
+    }
+
+    Node *somethingLinkedList = createLinkedList(something, n);
+    while (somethingLinkedList != NULL)
+    {
+        printf("%d, ", somethingLinkedList->value);
+        somethingLinkedList = somethingLinkedList->next;
     }
 }
 
@@ -46,5 +60,33 @@ void delNextNode(Node *node)
 {
     Node *temp = node->next->next;
     memset(node->next, 0, sizeof(Node));
-    node -> next = temp;
+    node->next = temp;
+}
+
+Node * createLinkedList(int *array, int size)
+{
+    if (size < 1)
+    {
+        return NULL;
+    }
+
+    Node *head = malloc(sizeof(Node));
+    Node *temp;
+    head->next = NULL;
+
+    int i = 0;
+    temp = head;
+
+    while (i < size-1)
+    {
+        temp->value = array[i];
+        temp->next = malloc(sizeof(Node));
+        temp = temp->next;
+        i++;
+    }
+
+    temp->value = array[i];
+    temp->next = NULL;
+
+    return head;
 }
