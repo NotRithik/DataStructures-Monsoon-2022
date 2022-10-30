8937429696205fe90c076206f7b27b689a6595bd
NotRithik <rithikum123@gmail.com>
2022-10-30 15:38:29 +0530

    Reverse LinkedList

--- LinkedList.c
+++ LinkedList.c
@@ -12,7 +12,8 @@ typedef struct LinkedList Node; // Define type Node
 
 void addNodeNext(int, Node *);
 void delNextNode(Node *);
-Node * createLinkedList(int *, int);
+Node *createLinkedList(int *, int);
+Node *reverseLinkedList(Node *);
 
 void main()
 {
@@ -34,13 +35,23 @@ void main()
     //     nodeA = nodeA->next;
     // }
 
-    int n = 10, something[n];
+    int n = 2, something[n];
     for (int i = 0; i < n; i++)
     {
-        something[i] = i+1;
+        something[i] = i + 1;
     }
 
     Node *somethingLinkedList = createLinkedList(something, n);
+    Node *head = somethingLinkedList;
+    while (somethingLinkedList != NULL)
+    {
+        printf("%d, ", somethingLinkedList->value);
+        somethingLinkedList = somethingLinkedList->next;
+    }
+
+    somethingLinkedList = head;
+
+    somethingLinkedList = reverseLinkedList(somethingLinkedList);
     while (somethingLinkedList != NULL)
     {
         printf("%d, ", somethingLinkedList->value);
@@ -50,10 +61,19 @@ void main()
 
 void addNodeNext(int value, Node *node)
 {
-    Node *temp = node->next;
-    node->next = malloc(sizeof(Node));
-    node->next->next = temp;
-    node->next->value = value;
+    if (node->next != NULL)
+    {
+        Node *temp = node->next;
+        node->next = malloc(sizeof(Node));
+        node->next->next = temp;
+        node->next->value = value;
+    }
+    else
+    {
+        node->next = malloc(sizeof(Node));
+        node->next->next = NULL;
+        node->next->value = value;
+    }
 }
 
 void delNextNode(Node *node)
@@ -63,30 +83,62 @@ void delNextNode(Node *node)
     node->next = temp;
 }
 
-Node * createLinkedList(int *array, int size)
+Node *createLinkedList(int *array, int size)
 {
-    if (size < 1)
+    if (size <= 0)
     {
         return NULL;
     }
 
     Node *head = malloc(sizeof(Node));
-    Node *temp;
     head->next = NULL;
+    head->value = array[0];
 
-    int i = 0;
+    Node *temp = head;
+
+    int i = 1;
     temp = head;
 
-    while (i < size-1)
+    while (i < size)
     {
-        temp->value = array[i];
-        temp->next = malloc(sizeof(Node));
+        addNodeNext(array[i], temp);
         temp = temp->next;
         i++;
     }
 
-    temp->value = array[i];
-    temp->next = NULL;
-
     return head;
+}
+
+Node *reverseLinkedList(Node *list)
+{
+
+    if (list == NULL || list->next == NULL) // If 0 or 1 element
+    {
+        return list;
+    }
+
+    if (list->next->next == NULL) { // If 2 elements
+        Node *temp = list->next;
+        list->next->next = list;
+        list->next = NULL;
+        return temp;
+    }
+
+    Node *secPrev = list;
+    Node *prev = list->next;
+    Node *current = list->next->next;
+
+    secPrev->next = NULL;
+
+    while (current != NULL)
+    {
+        prev->next = secPrev;
+
+        secPrev = prev;
+        prev = current;
+
+        current = current->next;
+    }
+
+    return current;
 }
