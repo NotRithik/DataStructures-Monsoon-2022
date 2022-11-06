8c2cbea45c000951cbb8addb7cd4db5b546b7fdb
NotRithik <rithikum123@gmail.com>
2022-11-06 17:35:03 +0530

    Label variables in header

--- LinkedList.h
+++ LinkedList.h
@@ -9,12 +9,12 @@ struct LinkedList
     struct LinkedList *next;
 };
 
-typedef struct LinkedList *Node; // Define type Node
+typedef struct LinkedList * Node; // Define type Node
 
-void addNodeNext(int, Node);
+void addNodeNext(int value, Node);
 void delNextNode(Node);
-Node createLinkedList(int *, int);
-Node reverseLinkedList(Node);
-Node chunkReverseLinkedList(Node, int);
-Node reverseChunk(Node, int);
-Node findElementN(Node, int);
+Node createLinkedList(int *array, int size);
+Node reverseLinkedList(Node list);
+Node chunkReverseLinkedList(Node list, int chunkSize);
+Node reverseChunk(Node list, int chunkSize);
+Node findElementN(Node list, int index);
